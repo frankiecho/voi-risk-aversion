b446d490fb0b95a5d33d1b1c246a346e34df9648
Frankie Cho <frankiechoht@gmail.com>
2024-03-28 09:28:43 +1000

    include raw calculations for v_uncertainty and v_certainty

--- code/voi_problems.R
+++ code/voi_problems.R
@@ -28,7 +28,7 @@ e1 <- list()
 n_states <- 2
 n_actions <- 2
 a1 <- c(1, 1)
-a2 <- c(1.5, 0.9)
+a2 <- c(1.12, 0.9)
 e1$p <- c(0.5, 0.5)
 action_state <- matrix(c(a1, a2), byrow = T, ncol = length(a1))
 colnames(action_state) <- paste0('s', 1:n_states)
@@ -66,45 +66,71 @@ set.seed(11111)
 n_states <- 50
 n_actions <- 50
 
-nsims <- 500
+
 
 ## Draw from uniform distribution like Holden et al (2024) ------
 plan(multisession)
 unif_action_state_sim <- function() matrix(runif(n_states*n_actions), nrow = n_actions)
 unif_plt <- fcn_plot_simulations(unif_action_state_sim)
-unif_plt
 
 ## Draw from an exponential distribution like Holden et al (2024) ------
 exp_action_state_sim <- function() matrix(rexp(n_states*n_actions, 1), nrow = n_actions)
 exp_plt <- fcn_plot_simulations(exp_action_state_sim)
-exp_plt
 
 ## Draw from an poisson distribution with heterogeneous rate parameters ------
 pois_action_state_sim <- function() rpois(n_states*n_actions, rep(sample(0:5, n_actions, replace = T), each=n_states)) %>%
   matrix(nrow = n_actions, byrow = T)
 pois_plt <- fcn_plot_simulations(pois_action_state_sim)
-pois_plt
 
 ## Draw from an lognormal distribution like Holden et al (2024) ------
 lnorm_action_state_sim <- function() rlnorm(n_states*n_actions, rep(0.1, each=n_states)) %>%
   matrix(nrow = n_actions, byrow = T)
 lnorm_plt <- fcn_plot_simulations(lnorm_action_state_sim)
-lnorm_plt
 
 ## Draw from a negative lognormal distribution ------
 neg_lnorm_action_state_sim <- function() -rlnorm(n_states*n_actions, rep(0.1, each=n_states)) %>%
   matrix(nrow = n_actions, byrow = T)
 neg_lnorm_plt <- fcn_plot_simulations(neg_lnorm_action_state_sim)
-neg_lnorm_plt
+
+## Draw from an kurtotic distribution ------
+t_action_state_sim <- function() rt(n_states*n_actions, 3) %>%
+  matrix(nrow = n_actions, byrow = T)
+t_plt <- fcn_plot_simulations(t_action_state_sim)
+
+## No trade-off in mean and variance
+mu <- seq(1, 1.5, length.out = n_actions)
+sd <- seq(0.5, 0.5, length.out = n_actions)
+mv_no_tradeoff_action_state_sim <- function() rnorm(n_states*n_actions, 
+                                        rep(mu, each=n_states),
+                                        rep(sd, each=n_states)) %>%
+  matrix(nrow = n_actions, byrow = T)
+mv_no_tradeoff_plt <- fcn_plot_simulations(mv_no_tradeoff_action_state_sim)
 
 ## Explicit trade-off in mean and variance
 mu <- seq(1, 1.5, length.out = n_actions)
-sd <- seq(0.1, 2, length.out = n_actions)
+sd <- seq(0.1, 3, length.out = n_actions)
 mv_action_state_sim <- function() rnorm(n_states*n_actions, 
                                           rep(mu, each=n_states),
                                           rep(sd, each=n_states)) %>%
   matrix(nrow = n_actions, byrow = T)
 mv_plt <- fcn_plot_simulations(mv_action_state_sim)
-mv_plt
+
+## Write plots -----
+fig1 <- unif_plt$order_plt + ggtitle("Uniform") + unif_plt$v_plt + unif_plt$plt + 
+  exp_plt$order_plt + ggtitle("Exponential") + exp_plt$v_plt + exp_plt$plt +
+  pois_plt$order_plt + ggtitle("Poisson") + pois_plt$v_plt + pois_plt$plt + plot_layout(guides='collect',byrow = F) & theme(legend.position = "bottom")
+fig1
+ggsave(fig1, filename = "plots/homogeneous_dist.png", width = 12, height = 10, dpi = 300)
+
+fig2 <- lnorm_plt$order_plt + ggtitle("Lognormal") + lnorm_plt$v_plt + lnorm_plt$plt + 
+  neg_lnorm_plt$order_plt + ggtitle("Lognormal (negative values)") + neg_lnorm_plt$v_plt + neg_lnorm_plt$plt + 
+  t_plt$order_plt + ggtitle("T-distribution") + t_plt$v_plt + t_plt$plt +
+  plot_layout(guides='collect',byrow = F, ncol = 3) & theme(legend.position = "bottom")
+fig2
+ggsave(fig2, filename = "plots/lnorm_dist.png", width = 12, height = 10, dpi = 300)
 
 
+fig3 <- mv_no_tradeoff_plt$order_plt + ggtitle("Normal distribution") + mv_no_tradeoff_plt$v_plt + mv_no_tradeoff_plt$plt +
+  mv_plt$order_plt + ggtitle("Mean-Variance tradeoff") + mv_plt$v_plt + mv_plt$plt + plot_layout(guides='collect',byrow = F, ncol=2) & theme(legend.position = "bottom")
+fig3
+ggsave(fig3, filename = "plots/mv_dist.png", width = 10, height = 10, dpi = 300)
--- code/voi_simulations.R
+++ code/voi_simulations.R
@@ -6,6 +6,10 @@ library(future)
 library(future.apply)
 library(Rmpfr)
 
+nsims <- 500
+
+okabe_ito_colors = c("#E69F00", "#56B4E9", "#009E73", "#CC79A7", "#D55E00", "#F0E442", "#0072B2",   "#999999")
+
 # vNM Utility functions
 CRRA <- function(c, gamma = 1) {
   if (gamma == 1) {
@@ -28,23 +32,23 @@ CRRA_inv <- function(c, gamma = 1) {
 }
 
 CARA <- function(c, alpha = 0) {
+  c <- c + 1e-6 # Adjustment for numeric stability
   if (alpha == 0 | is.na(alpha)) {
-    return(c)
+    res <- c
   } else {
-    return((1-exp(-alpha*c))/alpha)
+    res <- (1-exp(-alpha*c))/alpha
   }
+  return(res)
 }
 
 CARA_inv <- function(c, alpha = 0) {
+  c <- c - 1e-6 # Adjustment for numeric stability
   if (alpha == 0 | is.na(alpha)) {
-    return(c)
+    res <- c
   } else {
-    res <- -(log(1-alpha*c))/alpha
-    if (is.na(res)) {
-      return(res)
-    }
-    return(res)
+    res <- -(log(1-alpha*(c)))/alpha
   }
+  return(res)
 }
 
 # Expected Value of perfect information (compared in certainty equivalents)
@@ -55,7 +59,9 @@ fcn_EVPI <- function(gamma, action_state, p) {
   max_a <- apply(utility_table, 2, which.max) %>% unlist() # Actions that maximise utility in each state (indices)
   max_EU <- which.max(utility_table %*% p) # Action that maximises expected utility (index)
   VPI_value <- action_state[cbind(max_a, seq_along(max_a))] %*% p - action_state[max_EU,] %*% p
-  VPI <- U_inv(utility_table[cbind(max_a, seq_along(max_a))] %*% p) - U_inv(utility_table[max_EU,] %*% p)
+  V_certainty <- U_inv(utility_table[cbind(max_a, seq_along(max_a))] %*% p)
+  V_uncertainty <- U_inv(utility_table[max_EU,] %*% p)
+  VPI <- V_certainty - V_uncertainty
   if (any(is.na(VPI))) {
     warning('Some EVPI values are NA')
   }
@@ -65,11 +71,13 @@ fcn_EVPI <- function(gamma, action_state, p) {
   
   action_best_outcomes <- apply(action_state, 1, max)
   max_EU_best <- 1 + n_actions - match(action_best_outcomes[max_EU], sort(action_best_outcomes))
-  
-  action_mean_outcomes <- apply(action_state, 1, mean)
+
+  action_mean_outcomes <- action_state %*% p
   max_EU_mean <- 1 + n_actions - match(action_mean_outcomes[max_EU], sort(action_mean_outcomes))
   
   list(VPI = VPI, VPI_value = VPI_value, max_EU = max_EU, 
+       V_certainty = V_certainty,
+       V_uncertainty = V_uncertainty,
        action_worst_outcomes = max_EU_worst,
        action_mean_outcomes = max_EU_mean,
        action_best_outcomes = max_EU_best)
@@ -103,7 +111,9 @@ fcn_VOI_simulation  <- function(voi_problem, gamma_seq) {
     lapply(function(x) c(VPI = x$VPI, VPI_value = x$VPI_value, max_EU = x$max_EU, 
                          action_best_outcomes = x$action_best_outcomes,
                          action_mean_outcomes = x$action_mean_outcomes,
-                         action_worst_outcomes = x$action_worst_outcomes)) %>%
+                         action_worst_outcomes = x$action_worst_outcomes,
+                         V_certainty = x$V_certainty,
+                         V_uncertainty = x$V_uncertainty)) %>%
     bind_rows(.id = 'gamma')
 
   evpi_seq$gamma <- gamma_seq
@@ -120,14 +130,13 @@ fcn_voi_simulation_distribution <- function(action_state_sim_func, n_y = 100, ga
     n_actions <- nrow(prob$action_state)
     colnames(prob$action_state) <- paste0('s', 1:n_states)
     rownames(prob$action_state) <- paste0('a', 1:n_actions)
-    p <- runif(n_states)
-    prob$p <- p/sum(p)
+    prob$p <- rep(1/n_states, n_states)
     prob$Y <- sort(rep(1:n_y, 1+n_states/n_y)[1:n_states])
     prob$evpxi <- n_y < n_states
     prob
   }
   sim <- replicate(nsims, fcn_sim(), simplify = F)
-  sim_results <- future_lapply(sim, fcn_VOI_simulation, gamma_seq = gamma_seq)
+  sim_results <- lapply(sim, fcn_VOI_simulation, gamma_seq = gamma_seq)
   sim_table <- sim_results %>%
     #lapply(function(x) mutate(x, VPI = VPI - x$VPI[x$gamma == 0])) %>%
     #lapply(function(x) x$VPI) %>%
@@ -155,12 +164,12 @@ fcn_plt_sim_table <- function(table, ribbon = TRUE, col = NULL, additional_gg = 
   if (ribbon) {
     plt <- plt +
       geom_ribbon(aes(ymin = llb, ymax = uub), fill = 'gray80') +
-      geom_ribbon(aes(ymin = lb, ymax = ub), fill = 'gray70')
+      geom_ribbon(aes(ymin = lb, ymax = ub), fill = 'gray60')
   }
   
   if (is.null(col)) {
     plt +
-      geom_line(color = 'gray20', size = 1) +
+      geom_line(color = 'gray20', linewidth = 1) +
       theme_pubr()
   } else {
     plt +
@@ -171,6 +180,7 @@ fcn_plt_sim_table <- function(table, ribbon = TRUE, col = NULL, additional_gg = 
 
 fcn_plot_simulations <- function(action_state) {
   order_var <- c('action_worst_outcomes','action_mean_outcomes','action_best_outcomes')
+  values_cert_uncert <- c("V_certainty", "V_uncertainty")
   action_sim <- fcn_voi_simulation_distribution(action_state)
   action_sim_table <- fcn_summarise_table(action_sim)
   action_sim_order <- lapply(order_var, \(x) fcn_summarise_table(action_sim, variable = x))
@@ -179,15 +189,29 @@ fcn_plot_simulations <- function(action_state) {
   plt <- fcn_plt_sim_table(action_sim_table, additional_gg = list(geom_vline(xintercept = 0, color = 'gray50'))) +
     scale_y_continuous("Certainty-equivalent\n Value of Perfect Information") +
     scale_x_continuous("Risk Aversion Coefficient")
+  
+  v_certainty <- lapply(values_cert_uncert, \(x) fcn_summarise_table(action_sim, variable = x))
+  names(v_certainty) <- values_cert_uncert
+  v_certainty <- v_certainty %>% bind_rows(.id = 'name')
+  v_plt <- v_certainty %>%
+    mutate(name = factor(name, levels = values_cert_uncert, labels = c('Value under certainty', 'Value under uncertainty'))) %>%
+    fcn_plt_sim_table(F, 'name', additional_gg = list(geom_vline(xintercept = 0, color = 'gray50'))) +
+    guides(color = guide_legend(title="")) +
+    scale_color_manual(values = okabe_ito_colors[4:5])+
+    scale_y_continuous("Certainty-equivalent\n Value") +
+    theme(axis.line.x = element_blank(), axis.title.x = element_blank(),
+          axis.text.x = element_blank(), axis.ticks.x = element_blank())
+  
   order_plt <- action_sim_order %>%
     mutate(name = factor(name, levels = order_var, labels = c('Minimum', 'Mean', 'Maximum'))) %>%
     fcn_plt_sim_table(F, 'name', additional_gg = list(geom_hline(yintercept = 1, color = 'gray50'),
                                                       geom_vline(xintercept = 0, color = 'gray50'))) +
     scale_y_reverse("Rank") +
     guides(color = guide_legend(title="Order payoffs by")) +
+    scale_color_manual(values = okabe_ito_colors[1:3])+
     theme(axis.line.x = element_blank(), axis.title.x = element_blank(),
           axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
     annotate("text", x = -2.5, y = 0, label = "Risk-loving", vjust = 0, hjust = 0.5)+
     annotate("text", x = 2.5, y = 0, label = "Risk-averse", vjust = 0, hjust = 0.5)
-  list(order_plt = order_plt, plt = plt)
+  list(order_plt = order_plt, v_plt = v_plt, plt = plt)
 }
